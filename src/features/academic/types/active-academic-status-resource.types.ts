593f4fe26b50488fc92d0359d7a17d0b6bb8fdc8
Matías Delgado <matiasdelgadodev@gmail.com>
2026-08-18 22:39:08 -0300

    style(ui): format develop sources

--- src/features/academic/types/active-academic-status-resource.types.ts
+++ src/features/academic/types/active-academic-status-resource.types.ts
@@ -1,6 +1,4 @@
 import { AcademicResource } from "@features/academic/types/academic-resource.types";
 
 export type ActiveAcademicStatusResource =
-  | AcademicResource.TRAINING_PATH
-  | AcademicResource.ACADEMIC_SPACE
-  | AcademicResource.INSTRUMENT;
+  AcademicResource.TRAINING_PATH | AcademicResource.ACADEMIC_SPACE | AcademicResource.INSTRUMENT;
